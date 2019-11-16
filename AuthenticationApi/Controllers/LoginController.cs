4deb69da84dd7e18d5dacc4d0667a069e7a7e94a
Sudhakar <sssudhakar1985@gmail.com>
2019-11-16 17:34:00 +0000

    Authentication Api added

---
+++ AuthenticationApi/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿namespace AuthenticationApi.Controllers
+{
+    using AuthenticationApi.Model;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // POST api/authentication
+        [HttpPost, Route("")]
+        public User Post([FromBody] Login login)
+        {
+            return new User
+            {
+                Name = login.Username,
+                Email = $"{login.Username}@abc.de",
+                Token = "test"
+            };
+        }
+    }
+}
---
+++ AuthenticationApi/Model/User.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthenticationApi.Model
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Token { get; set; }
+    }
+}
