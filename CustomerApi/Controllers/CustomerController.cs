6103604460d8b4dede9fa064e1253cd8b4bd3ef3
Sudhakar <sssudhakar1985@gmail.com>
2019-11-16 16:28:33 +0000

    initial commit

---
+++ CustomerApi/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿namespace CustomerApi.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        // GET api/customers
+        [HttpGet, Route("")]
+        public ActionResult<IEnumerable<Customer>> Get()
+        {
+            return new Customer[] { new Customer("John"), new Customer("Crish") };
+        }
+    }
+
+    public class Customer
+    {
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ EmployeeApi/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿namespace EmployeeApi.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        // GET api/employees
+        [HttpGet, Route("")]
+        public ActionResult<IEnumerable<Employee>> Get()
+        {
+            return new Employee[] { new Employee("John"), new Employee("Crish") };
+        }
+    }
+
+    public class Employee
+    {
+        public Employee(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+
+}
